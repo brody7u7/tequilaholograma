fe6dc57808994c11f31c651258bf355ee9f8a808
Carlos Luevano <carlos@sectorweb.mx>
2019-03-21 18:23:02 -0600

    Menu arrastrable

---
+++ Assets/Scripts/DragHandler.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DragHandler : MonoBehaviour, IPointerClickHandler, IBeginDragHandler, IDragHandler, IEndDragHandler {
+
+    private MenuBehaviour _menuBehaviour;
+    private bool _isDragging;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    public void setMenuBehaviour(MenuBehaviour menuBehaviour)
+    {
+        _menuBehaviour = menuBehaviour;
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        if (_menuBehaviour == null || _isDragging) return;
+        // VERIFICA SI DEBE CERRAR EL MENU
+        if (_menuBehaviour.IsMenuOpen)
+            _menuBehaviour.CloseMenu();
+
+        Debug.Log("Pointer Click");
+    }
+
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        Debug.Log("Begin Drag");
+        _isDragging = true;
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        Debug.Log(eventData.position);
+        if (_menuBehaviour != null)
+            _menuBehaviour.ChangeMenuPosition(eventData.position);
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        Debug.Log("End Drag");
+        _isDragging = false;
+    }
+}
--- Assets/Scripts/MenuBehaviour.cs
+++ Assets/Scripts/MenuBehaviour.cs
@@ -9,62 +9,108 @@ public class MenuBehaviour : MonoBehaviour {
     private const float OPEN_POSITION_X = 0f;
     private const float CLOSE_POSITION_X = -750f;
 
-    private RectTransform rectTransform;
+    private RectTransform _rectTransform;
 
-    private float startPositionX;
-    private float stepsPosition;
+    private float _screenWidth;
+    private float _menuWith;
+    private float _stepsPosition;
+
+    public bool IsMenuOpen { private set; get; }
 
 	// Use this for initialization
-	void Start () {
-        rectTransform = GetComponent<RectTransform>();
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
-
-    public void openMenu()
+	void Start ()
     {
-        StartCoroutine(AnimateOpenMenu());
+        _rectTransform = GetComponent<RectTransform>();
+        _screenWidth = Screen.width;
+        _menuWith = gameObject.transform.Find("Navigation Drawer").GetComponent<RectTransform>().sizeDelta.x;
+
+        var drag = GetComponentInChildren<DragHandler>();
+        if (drag != null)
+            drag.setMenuBehaviour(this);
+    }
+	
+    public void OpenMenu()
+    {
+        OpenMenu(true);
     }
 
-    public void closeMenu()
+    public void CloseMenu()
     {
-        StartCoroutine(AnimateCloseMenu());
+        CloseMenu(true);
+    }
+
+    public void OpenMenu(bool animate)
+    {
+        if (animate)
+        {
+            StartCoroutine(AnimateOpenMenu());
+        }
+        else
+        {
+
+        }
+    }
+
+    public void CloseMenu(bool animate)
+    {
+        if (animate)
+        {
+            StartCoroutine(AnimateCloseMenu());
+        }
+        else
+        {
+
+        }
+    }
+
+    public void ChangeMenuPosition(Vector2 dragValue)
+    {
+        float distance = Mathf.Abs(_menuWith - dragValue.x);
+        Vector2 position = _rectTransform.anchoredPosition;
+        position.x -= position.x + distance;
+        _rectTransform.anchoredPosition = position;
+        //Debug.Log(position);
+        //Debug.Log(_menuWith);
     }
 
     IEnumerator AnimateOpenMenu()
     {
-        float x = rectTransform.anchoredPosition.x;
+        float x = _rectTransform.anchoredPosition.x;
         float step = Mathf.Abs((x - OPEN_POSITION_X) * ANIMATION_WAIT / ANIMATION_DURATION);
         for (; x <= 0; x += step)
         {
-            Vector2 position = rectTransform.anchoredPosition;
+            Vector2 position = _rectTransform.anchoredPosition;
             position.x = x;
-            rectTransform.anchoredPosition = position;
+            _rectTransform.anchoredPosition = position;
             yield return new WaitForSeconds(ANIMATION_WAIT);
         }
 
-        Vector2 position2 = rectTransform.anchoredPosition;
+        Vector2 position2 = _rectTransform.anchoredPosition;
         position2.x = OPEN_POSITION_X;
-        rectTransform.anchoredPosition = position2;
+        _rectTransform.anchoredPosition = position2;
+
+        _rectTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, _screenWidth);
+        IsMenuOpen = true;
     }
 
     IEnumerator AnimateCloseMenu()
     {
-        float x = rectTransform.anchoredPosition.x;
+        float x = _rectTransform.anchoredPosition.x;
         float step = Mathf.Abs((CLOSE_POSITION_X - x) * ANIMATION_WAIT / ANIMATION_DURATION);
         for (; x >= CLOSE_POSITION_X; x -= step)
         {
-            Vector2 position = rectTransform.anchoredPosition;
+            Vector2 position = _rectTransform.anchoredPosition;
             position.x = x;
-            rectTransform.anchoredPosition = position;
+            _rectTransform.anchoredPosition = position;
             yield return new WaitForSeconds(ANIMATION_WAIT);
         }
 
-        Vector2 position2 = rectTransform.anchoredPosition;
+        Vector2 position2 = _rectTransform.anchoredPosition;
         position2.x = CLOSE_POSITION_X;
-        rectTransform.anchoredPosition = position2;
+        _rectTransform.anchoredPosition = position2;
+
+        _rectTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, 790f);
+        IsMenuOpen = false;
     }
+
 }
